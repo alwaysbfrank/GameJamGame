812ac6b9fce38eefadf92cbc597cb3780d8496e6
Grerek <grzegorz.brodzisz.2@gmail.com>
2021-11-27 17:31:15 +0100

    MenuBasic

---
+++ Assets/Scripts/CreditsAndInstruction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsAndInstruction : MonoBehaviour
+{
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
