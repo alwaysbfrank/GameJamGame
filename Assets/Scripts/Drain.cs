6072b4afa2d9aeeabb4350ae4251a6cbdf4228e2
Grerek <grzegorz.brodzisz.2@gmail.com>
2021-11-27 21:23:57 +0100

    Kratka

---
+++ Assets/Scripts/Drain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drain : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerGas"))
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+    }
+}
