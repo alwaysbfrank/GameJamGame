dc6e542ac47255475ee3b5cf5a9966e7b7ce1dbc
Grerek <grzegorz.brodzisz.2@gmail.com>
2021-11-27 22:54:26 +0100

    Zmiany

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("PlayerRock") || collision.gameObject.CompareTag("PlayerGas"))
+        {
+            SceneManager.LoadScene("Nazwa sceny");
+        }
+    }
+}
