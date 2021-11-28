1bd6c01ec5248789f13b0472072de53f1c1ce778
Franek <franek.matera@gmail.com>
2021-11-28 01:18:08 +0100

    volume script

---
+++ Assets/Scripts/VolumeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeScript : MonoBehaviour
+{
+    [Range(0, 100)] public float hearingDistance;
+    public GameObject player;
+    private AudioSource _audio;
+
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        var distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer > hearingDistance)
+        {
+            _audio.volume = 0f;
+            return;
+        }
+
+        _audio.volume = 1 - distanceToPlayer / hearingDistance;
+    }
+}
