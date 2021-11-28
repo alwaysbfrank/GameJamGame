277cb881c64a89eee05de96019b9733fe328486f
Grerek <grzegorz.brodzisz.2@gmail.com>
2021-11-28 11:00:56 +0100

    Lvl 01

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public string sceneToName;
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("PlayerRock") || collision.gameObject.CompareTag("PlayerGas"))
         {
