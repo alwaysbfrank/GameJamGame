3653ca2023a325cb196b31167fba97069be38d84
Franek <franek.matera@gmail.com>
2021-11-28 10:01:41 +0100

    fix VolumeScript.cs

--- Assets/Scripts/VolumeScript.cs
+++ Assets/Scripts/VolumeScript.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VolumeScript : MonoBehaviour
 {
     [Range(0, 100)] public float hearingDistance;
-    public GameObject player;
+    public GameObject humanPlayer;
+    public GameObject stonePlayer;
+    public GameObject gasPlayer;
+    private GameObject activePlayer;
     private AudioSource _audio;
 
     void Start()
@@ -13,9 +17,41 @@ public class VolumeScript : MonoBehaviour
         _audio = GetComponent<AudioSource>();
     }
 
+    private void OnEnable()
+    {
+        activePlayer = humanPlayer;
+        PlayerSwitcherEventSystem.FromGasToHuman += HumanActive;
+        PlayerSwitcherEventSystem.FromHumanToGas += GasActive;
+        PlayerSwitcherEventSystem.FromHumanToStone += StoneActive;
+        PlayerSwitcherEventSystem.FromStoneToHuman += HumanActive;
+    }
+
+    private void OnDisable()
+    {
+        PlayerSwitcherEventSystem.FromGasToHuman -= HumanActive;
+        PlayerSwitcherEventSystem.FromHumanToGas -= GasActive;
+        PlayerSwitcherEventSystem.FromHumanToStone -= StoneActive;
+        PlayerSwitcherEventSystem.FromStoneToHuman -= HumanActive;
+    }
+
+    public void HumanActive()
+    {
+        activePlayer = humanPlayer;
+    }
+
+    public void GasActive()
+    {
+        activePlayer = gasPlayer;
+    }
+
+    public void StoneActive()
+    {
+        activePlayer = stonePlayer;
+    }
+
     void Update()
     {
-        var distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        var distanceToPlayer = Vector3.Distance(activePlayer.transform.position, transform.position);
         if (distanceToPlayer > hearingDistance)
         {
             _audio.volume = 0f;
